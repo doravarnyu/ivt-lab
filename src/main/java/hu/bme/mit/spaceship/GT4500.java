c52a8d9d768021e365bd362af6ffd697cd880f20
Dóra Varnyú <varnyu.dora@gmail.com>
2019-04-12 09:12:31 +0200

    Changed parameter name of fireLaser from firingMode to laserFiringMode

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -15,7 +15,7 @@ public class GT4500 implements SpaceShip {
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
 
-  public boolean fireLaser(FiringMode firingMode) {
+  public boolean fireLaser(FiringMode laserFiringMode) {
     // TODO not implemented yet
     return false;
   }
